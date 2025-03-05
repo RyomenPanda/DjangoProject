75868e387ae8978c5e14c27c9aeaa85a6a75f370
Panda <91757462+RyomenPanda@users.noreply.github.com>
2025-03-05 19:06:54 +0530

    Added Files
    Added project files to the repository.

---
+++ app1/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from .forms import SignUpForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
+    
+def home(request):
+    return render(request, 'home.html')
